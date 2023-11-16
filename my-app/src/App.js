8f74b4070d7c45d797abeb58079e309f5dd4c23f
DylanMiller765 <Dylanjaws185323@gmail.com>
2023-11-16 14:13:06 -0600

    HomePage and Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,12 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('./Pages/home/Home'));
+const NavBar = lazy(() => import('./Components/NavBar/NavBar'));
 
 function App() {
 	return (
 		<Router className="app">
+			<NavBar/>
 			<Routes>
 				<Route path="/" element={<Home />} />
 				{/* <Route path="/app" eleme /> */}
--- my-app/src/Pages/home/Home.js
+++ my-app/src/Pages/home/Home.js
@@ -1,8 +1,78 @@
-import React from 'react'
-//import './home.scss'
-//Const Home = () +
-export default function Home() {
+import React, { useState } from 'react';
+import { Col, Container, Row, Dropdown } from 'react-bootstrap';
+import './home.scss';
+
+const Home = () => {
+  const [selectedLanguage1, setSelectedLanguage1] = useState(null);
+  const [selectedLanguage2, setSelectedLanguage2] = useState(null);
+
+  const handleLanguageChange1 = (language) => {
+    console.log(`Selected language 1: ${language}`);
+    setSelectedLanguage1(language);
+  };
+
+  const handleLanguageChange2 = (language) => {
+    console.log(`Selected language 2: ${language}`);
+    setSelectedLanguage2(language);
+  };
+
+  const languages = ["English", "Spanish", "French", "German", "Chinese"]; // Add your supported languages
+
   return (
-	 <div>home</div>
-  )
-}
+    <Container fluid className="contained HomePage">
+      <Row className="pb-5">
+        <Col md="6">
+          <div className="Home-Page">
+            <h1>LASSO</h1>
+            <p className="lead-text">
+              Learn a new language with ease.
+            </p>
+            
+            <div className="learn-dropdown-container">
+              <span className="learn-text">I want to learn</span>
+              <Dropdown>
+                <Dropdown.Toggle variant="secondary" id="dropdownLanguage1">
+                  {selectedLanguage1 ? selectedLanguage1 : "Select Language 1"}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  {languages.map((language, index) => (
+                    <Dropdown.Item
+                      key={index}
+                      onClick={() => handleLanguageChange1(language)}
+                    >
+                      {language}
+                    </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+
+              {/* Second Dropdown with added spacing */}
+              <div className="learn-dropdown-container" style={{ marginLeft: '100px' }}>
+                <span className="learn-text">I can teach</span>
+                <Dropdown>
+                  <Dropdown.Toggle variant="secondary" id="dropdownLanguage2">
+                    {selectedLanguage2 ? selectedLanguage2 : "Select Language 2"}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    {languages.map((language, index) => (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => handleLanguageChange2(language)}
+                      >
+                        {language}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
